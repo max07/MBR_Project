f2e5fea78733606ecf7da4c71a52089ef6c010a4
Manoj Kalaskar <max07@users.noreply.github.com>
2015-04-20 21:24:53 +0530

    Update recommender.m
    v2

--- recommender.m
+++ recommender.m
@@ -1,35 +1,36 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%% Critic Rating %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 prompt = 'Movie Recommender \nEnter type of Critic rating: 0.No Rating 1.Minimum 2.Maximum 3.Around \n';
 rtype = input(prompt);
-
+flagC = 0;
+flagA = 0;
 if rtype ~= 0
     prompt = 'Enter Rating: \n';
     rating = input(prompt);
     %disp(rating);
-
+    flagC = 1;
     %disp(rtype);
-    ratingFilter = ones(10197,2);
-    ratingFilter = ratingFilter * 10;
-    ratingArray = zeros(10197,1);
+    ratingFilterC = ones(10197,2);
+    ratingFilterC = ratingFilterC * 10;
+    ratingArrayC = zeros(10197,1);
 
-    ratingArray = rtAllCriticsRating;
+    ratingArrayC = rtAllCriticsRating;
  
     for i = 1:10197
-        ratingFilter(i,1) = abs(rating - ratingArray(i));
-        ratingFilter(i,2) = i;
+        ratingFilterC(i,1) = abs(rating - ratingArrayC(i));
+        ratingFilterC(i,2) = i;
     end
     
     if rtype == 1    
           for i = 1:10197
-             if ratingArray(i) >= rating
-                ratingFilter(i,1) = 0;         
+             if ratingArrayC(i) >= rating
+                ratingFilterC(i,1) = 0;         
              end   
         end
 
     elseif rtype == 2    
           for i = 1:10197
-              if ratingArray(i) <= rating
-                ratingFilter(i,1) = 0;         
+              if ratingArrayC(i) <= rating
+                ratingFilterC(i,1) = 0;         
               end   
           end
     end    
@@ -47,7 +48,7 @@ if rtype ~= 0
     prompt = 'Enter Rating: \n';
     rating = input(prompt);
     %disp(rating);
-
+    flagA = 1;
     %disp(rtype);
     ratingFilterA = ones(10197,2);
     ratingFilterA = ratingFilterA * 10;
@@ -76,3 +77,32 @@ if rtype ~= 0
     end    
 
 end
+%%%%%%%%%%%
+
+%flagC
+%flagA
+%%%%%%%%%%%%%%%%%% User and/or Audience %%%%%%%%%%%%%%
+
+if flagC == 1 && flagA == 1
+    prompt = 'Enter choice: 1.Critic AND Audience 2.Critic OR Audience\n';
+    rop = input(prompt);
+    
+    if rop == 1
+        ratingOut = ratingFilterC + ratingFilterA /2;
+    elseif rop == 2
+        ratingOut = min(ratingFilterC,ratingFilterA);
+    end
+    
+    for i = 1 : 10197
+        ratingIndex(i,1) = ratingOut(i);
+        ratingIndex(i,2) = i;
+    end    
+    result = sortrows(ratingIndex);
+    disp('The recommended Movies are: \n');
+    i = 2;
+    while( i <= 10197 && (result(i-1,1) == result(i,1)))
+        disp(title(result(i-1,2)));
+        i = i+1;
+    end
+end
+%%%%%%%%%%%%
